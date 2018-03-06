123421f410d485e33c10534a9a831e1c51542fab
Nick Lee <nickoless.lee@gmail.com>
2018-03-06 21:25:42 +0000

    Added feature test to determine if clicking on a products redirects to product page

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module Jungle
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.secret_key_base = 'blahblahblah'
+
     config.generators do |g|
       g.skip_routes  true
       g.helper       false
---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Clicking on a product redirects to the product page" do
+    visit root_path
+
+    first('.product').find_link('Details').click
+     
+    save_and_open_screenshot('product-test.png')
+  end
+
+end
